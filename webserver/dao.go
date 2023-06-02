4dca1a374b80748428ec2316076019e5d52c2437
xin <xin@toppath.tw>
2023-06-02 17:44:55 +0800

    取得會員資料

--- webserver/dao.go
+++ webserver/dao.go
@@ -8,3 +8,15 @@ type LoginReq struct {
 type LoginRes struct {
 	Token string `json:"token"`
 }
+
+type MemberReq struct {
+	Token string `json:"token"`
+}
+
+type MemberRes struct {
+	TodayBet int64 `json:"todayBet"`
+	TodayPay int64 `json:"todayPay"`
+	Status   int   `json:"status"`
+	TotalBet int64 `json:"totalBet"`
+	TotalPay int64 `json:"totalPay"`
+}
--- webserver/hendler_post.go
+++ webserver/hendler_post.go
@@ -8,6 +8,7 @@ import (
 
 func (w *WebServer) initPost() {
 	w.login()
+	w.getMemberData()
 }
 
 func (w *WebServer) login() {
@@ -28,6 +29,27 @@ func (w *WebServer) login() {
 
 		res := new(LoginRes)
 		res.Token = "success Token"
+
+		c.JSON(200, res)
+	})
+}
+
+func (w *WebServer) getMemberData() {
+	w.router.POST("/api/getMemberData", func(c *gin.Context) {
+		req := new(MemberReq)
+
+		if err := c.ShouldBindJSON(req); err != nil {
+			fmt.Println(err, req)
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		res := new(MemberRes)
+		res.TodayBet = 1
+		res.TodayPay = 2
+		res.Status = 3
+		res.TotalBet = 4
+		res.TotalPay = 5
 
 		c.JSON(200, res)
 	})
